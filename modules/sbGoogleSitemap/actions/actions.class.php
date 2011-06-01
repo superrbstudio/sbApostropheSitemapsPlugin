25d18e5ef61b5f29d8e0c97dbd823d8826a8d25d
Giles Smith <giles@superrb.com>
2011-06-01 11:15:59 +0100

    Updated to output xml of pages

--- modules/sbGoogleSitemap/actions/actions.class.php
+++ modules/sbGoogleSitemap/actions/actions.class.php
@@ -9,31 +9,96 @@
  */
 class sbGoogleSitemapActions extends BaseaActions
 {
-	protected $pages = array();
+	public $sitemapPages = array();
+	protected $defaultPriority = 0.8;
+	protected $settings = null;
+	protected $domain = null;
 	
+	/**
+	 * Execute the sitemap action
+	 * 
+	 * @param sfWebRequest $request
+	 */
 	public function executeSitemap(sfWebRequest $request)
-	{
-		$this->getResponse()->setContentType('text/xml');  
-		$root = aPageTable::retrieveBySlug('/');
-		$this->createSitemapPages($root->getTreeInfo());
+	{	
+		$this->settings = sfConfig::get('app_a_sbGoogleSitemap');
+		$this->getResponse()->setContentType('text/xml'); 
 		
-		var_dump($treeInfo);
+		// set the domain
+		$this->domain = $request->getHost();
+		
+		// add the homepage to the sitemap
+		$this->createHomepage();
+		
+		// add all apostrophe pages
+		$root = aPageTable::retrieveBySlug('/');
+		$this->createSitemapPagesFromPages($root->getTreeInfo());
+		
+		$this->outputPages = $this->sitemapPages;
 	}
 	
-	protected function createSitemapPages($array)
-	{
-		var_dump($array);
-		
+	/**
+	 * Turn page tree into page objects for xml
+	 * 
+	 * @param array $array Apostrophe page tree
+	 */
+	protected function createSitemapPagesFromPages($array)
+	{	
 		foreach($array as $p)
 		{
-			if(is_array($p))
+			if(isset($p['children']) and is_array($p['children']))
 			{
-				$this->createSitemapPages($p);
+				$this->createSitemapPagesFromPages($p['children']);
 			}
 			else
-			{
-				//$url = 
+			{	
+				if($p['view_guest'] == 1)
+				{
+					if($p['view_is_secure'] == 1) { $secure = TRUE; } else { $secure = FALSE; }
+					$this->createPage($p['slug'], $secure, array('priority_setting_name' => 'priority'));
+				}
 			}
 		}
+	}
+	
+	/**
+	 * Create the homepage
+	 */
+	protected function createHomepage()
+	{
+		$this->createPage('/', FALSE, array('priority_setting_name' => 'homepage_priority'));
+	}
+	
+	/**
+	 * Create pages for the pages array
+	 * 
+	 * @param string $slug
+	 * @param boolean $isSecure
+	 * @param array $params 
+	 */
+	protected function createPage($slug, $isSecure = FALSE, $params = array())
+	{
+		$page = new stdClass();
+					
+		// is secure
+		if($isSecure){ $page->uri = 'https://'; } else { $page->uri = 'http://'; }
+		
+		// construct uri
+		$page->uri .= $this->domain . $slug;
+		
+		// set the priority if it is available
+		if(is_numeric($this->settings[$params['priority_setting_name']]) and $this->settings[$params['priority_setting_name']] >= 0 and $this->settings[$params['priority_setting_name']] <= 1)
+		{
+			$page->priority = $this->settings[$params['priority_setting_name']];
+		}
+		else
+		{
+			$page->priority = $this->defaultPriority;
+		}
+		
+		// @TODO Insert datetime for modified
+		$page->updated = date('c');
+
+		$this->sitemapPages[] = $page;
 	}
 }
--- modules/sbGoogleSitemap/templates/sitemapSuccess.php
+++ modules/sbGoogleSitemap/templates/sitemapSuccess.php
@@ -5,3 +5,15 @@
  * and open the template in the editor.
  */
 ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"  
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">  
+  <?php foreach($outputPages as $page): ?>  
+  <url>  
+    <loc><?php echo $page->uri; ?></loc>  
+		<changefreq>daily</changefreq>
+		<priority><?php echo $page->priority; ?></priority> 
+    <lastmod><?php echo $page->updated; ?></lastmod> 
+  </url>  
+  <?php endforeach ?>  
+</urlset>  
