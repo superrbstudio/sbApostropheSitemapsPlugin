b704e23897a506bd60263d7cd8e892d6658b8995
Giles Smith <giles@superrb.com>
2011-07-26 16:37:18 +0100

    Removed hardcoded values

--- modules/sbGoogleSitemap/templates/sitemapSuccess.php
+++ modules/sbGoogleSitemap/templates/sitemapSuccess.php
@@ -5,15 +5,13 @@
  * and open the template in the editor.
  */
 ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"  
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  
-  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">  
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <?php foreach($outputPages as $page): ?>  
-  <url>  
-    <loc><?php echo $page->uri; ?></loc>  
-		<changefreq>daily</changefreq>
-		<priority><?php echo number_format($page->priority, 2); ?></priority> 
-    <lastmod><?php echo $page->updated; ?></lastmod> 
-  </url>  
+	<url>  
+		<loc><?php echo $page->getLoc(); ?></loc>  
+		<changefreq><?php echo $page->getChangeFreq(); ?></changefreq>
+		<priority><?php echo $page->getPriority(); ?></priority> 
+		<lastmod><?php echo $page->getLastmod(); ?></lastmod> 
+	</url>  
   <?php endforeach ?>  
 </urlset>  
